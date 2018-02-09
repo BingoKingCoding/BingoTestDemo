17fa373ed85d6c9457a7f7c7b5785c6fe1484a7b
leaderbean <634051075@qq.com>
2018-02-09 14:54:40 +0800

    提交代码(安卓开发技巧)

---
+++ app/src/main/java/com/bingo/king/devolopeskill/SkillActivity.java
@@ -0,0 +1,21 @@
+package com.bingo.king.devolopeskill;
+
+import android.os.Bundle;
+
+import com.bingo.king.BaseActivity;
+import com.bingo.king.R;
+
+/**
+ * https://mp.weixin.qq.com/s/wTOyYuMqkX9ezNvbixXquQ
+ */
+
+public class SkillActivity extends BaseActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skill);
+    }
+}
